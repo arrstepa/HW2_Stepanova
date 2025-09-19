598469056aecf8238007e03f99190c346f726b49
jallopukki <jallopukki@ya.ru>
2025-09-19 21:37:39 +0500

    Add division and Zero division message

--- HW2_Stepanova/calculator.py
+++ HW2_Stepanova/calculator.py
@@ -9,3 +9,10 @@ def substraction(a, b):
 def addition (a, b):
     "сложение двух чисел"
     return a + b
+
+def division (a, b):
+    "деление двух чисел"
+    if b!= 0:
+        return a/b
+    else:
+        return "деление на 0!"
